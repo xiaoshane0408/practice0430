8865a0ce146b54bd406c47c73ff0670823c529d4
xiaoshane0408 <63327406+xiaoshane0408@users.noreply.github.com>
2020-04-30 15:19:05 +0800

    practice other methods

---
+++ practice0430/Assets/control.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class control : MonoBehaviour
+{
+   
+    public monster bat;
+    public monster slime;
+    public Text message;
+    public Button batatk;
+    public Button batcure;
+    public Button slimeatk;
+    public Button slimecure;
+
+    private void Start()
+    {
+        batatk.onClick.AddListener(BatAttack);
+        batcure.onClick.AddListener(BatCure);
+        slimeatk.onClick.AddListener(SlimeAttack);
+        slimecure.onClick.AddListener(SlimeCure);
+    }
+
+    public void BatAttack()
+    {
+        // slime.hp = slime.hp - bat.atk;
+        slime.hp -= bat.atk;
+        message.text = slime.name + " - 受到傷害：" + bat.atk + "\n"
+                     + slime.name + " - 血量剩下：" + slime.hp;
+    }
+    public void SlimeAttack()
+    {
+        bat.hp -= slime.atk;
+        message.text = bat.name + " - 受到傷害：" + slime.atk + "\n"
+                      + bat.name + " - 血量剩下：" + bat.hp;
+    }
+    public void BatCure()
+    {
+        bat.hp += bat.cure;
+        message.text = bat.name + " - 收到治癒：" + bat.cure + "\n"
+                     + bat.name + " - 血量剩下：" + bat.hp;
+        
+    }
+    public void SlimeCure()
+    {
+        slime.hp += slime.cure;
+        message.text = slime.name + " - 收到治癒：" + slime.cure + "\n"
+                     + slime.name + " - 血量剩下：" + slime.hp;
+    }
+
+    //public void Hurt()
+    //{
+    //    if (batatk)
+    //    {
+    //        slime.hp -= bat.atk;
+    //        message.text = slime.name + " - 受到傷害：" + bat.atk + "\n"
+    //                      + slime.name + " - 血量剩下：" + slime.hp;
+    //    }
+    //    else if (slimeatk)
+    //    {
+    //        bat.hp -= slime.atk;
+    //        message.text = bat.name + " - 受到傷害：" + slime.atk + "\n"
+    //                      + bat.name + " - 血量剩下：" + bat.hp;
+    //    }
+
+    //}
+    //public void cCure()
+    //{
+    //    if (batcure)
+    //    {
+    //        bat.hp += bat.cure;
+    //        message.text = bat.name + " - 收到治癒：" + bat.cure + "\n"
+    //                      + bat.name + " - 血量剩下：" + bat.hp;
+    //    }
+    //    else if (slimecure)
+    //    {
+    //        slime.hp += slime.cure;
+    //        message.text = slime.name + " - 收到治癒：" + slime.cure + "\n"
+    //                      + slime.name + " - 血量剩下：" + slime.hp;
+    //    }
+    //}
+}
---
+++ practice0430/Assets/monster.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class monster : MonoBehaviour
+{
+    public int hp = 200;
+    public int atk = 30;
+    public int cure = 10;
+    
+}
